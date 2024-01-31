7eb20e53f9f734c8c2bc18e768c6fe690fd08843
Tarekbenm <tarek.bmd@gmail.com>
2024-01-31 11:07:12 +0100

    graph

--- streamlit.py
+++ streamlit.py
@@ -4,3 +4,5 @@ import pandas as pd
 link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/weather2019.csv"
 df_weather = pd.read_csv(link)
 st.write(df_weather)
+
+st.line_chart(df_weather['MAX_TEMPERATURE_C'])
