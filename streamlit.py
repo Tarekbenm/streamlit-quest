3e287f296d327073b4fdfe8bcbe44923f8b2ca71
Tarekbenm <tarek.bmd@gmail.com>
2024-01-31 11:18:43 +0100

    correlation

--- streamlit.py
+++ streamlit.py
@@ -6,3 +6,11 @@ df_weather = pd.read_csv(link)
 st.write(df_weather)
 
 st.line_chart(df_weather['MAX_TEMPERATURE_C'])
+
+import seaborn as sns
+viz_correlation = sns.heatmap(df_weather.corr(), 
+								center=0,
+								cmap = sns.color_palette("vlag", as_cmap=True)
+								)
+
+st.pyplot(viz_correlation.figure)
